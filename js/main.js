68d097530882ed90c3fc9b55ba2b2e9a5eb75fd0
Shivani Sanggam <s.sanggam@unasat.sr>
2026-07-17 20:53:53 -0300

    for test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const stars = document.getElementById("stars");
+
+for (let i = 0; i < 120; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.animationDuration = 2 + Math.random() * 6 + "s";
+    star.style.animationDelay = Math.random() * 6 + "s";
+    star.style.height = 40 + Math.random() * 120 + "px";
+    stars.appendChild(star);
+}
